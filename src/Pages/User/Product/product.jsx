6b8720508da9ace90d4569c648969c8f75e51d84
Khanh Deddo <khanhdeddo@gmail.com>
2024-12-28 19:24:32 +0700

    updatebykhanhdeddo

--- src/Pages/User/Product/product.jsx
+++ src/Pages/User/Product/product.jsx
@@ -249,8 +249,8 @@ const BookDetails = ({ onCartUpdated }) => {
   const responsive = {
     superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 7 },
     desktop: { breakpoint: { max: 3000, min: 1024 }, items: 6 },
-    tablet: { breakpoint: { max: 1024, min: 464 }, items: 4 },
-    mobile: { breakpoint: { max: 464, min: 0 }, items: 2 },
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 5 },
+    mobile: { breakpoint: { max: 464, min: 0 }, items: 5 },
   };
 
   if (!book) return <div>Đang tải chi tiết sách...</div>;
